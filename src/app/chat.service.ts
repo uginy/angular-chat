d4ca55dcc6156c5bdaec9357c174e2c4b8f55fbc
Ievgen Khoptiar <ievgen.khoptiar@gmail.com>
2020-04-15 20:37:26 +0300

    url api

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -6,8 +6,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ChatService {
-  // private url = 'https://my-mystychat.herokuapp.com';
-  private url = 'http://192.168.0.108:5000';
+  private url = 'https://my-mystychat.herokuapp.com';
+  // private url = 'http://192.168.0.108:5000';
   private socket = io(this.url);
 
   removeUser(user) {
